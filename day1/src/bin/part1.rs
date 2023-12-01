259b57df8c370647bb96607a94dd69ec89f0b2a8
Marco Meijer <marcomeijerm@gmail.com>
2023-12-01 12:01:40 +0100

    day 1: part 1

--- day1/src/bin/part1.rs
+++ day1/src/bin/part1.rs
@@ -4,6 +4,39 @@ fn main() {
     println!("{output}");
 }
 
-fn part1(s: &str) -> String {
-    todo!();
+fn part1(input: &str) -> String {
+    let mut res = 0;
+    for line in input.lines() {
+        let mut first: Option<char> = None;
+        let mut last: Option<char> = None;
+        for c in line.chars() {
+            if c >= '0' && c <= '9' {
+                if first.is_none() {
+                    first = Some(c);
+                }
+                last = Some(c);
+            }
+        }
+        let mut current = String::new();
+        current.push(first.unwrap());
+        current.push(last.unwrap());
+        res += current.parse::<i32>().unwrap();
+    }
+    format!("{res}")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        let result = part1(
+            "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet",
+        );
+        assert_eq!(result, "142");
+    }
 }
