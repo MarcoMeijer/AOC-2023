8e27380496bb84999c02a02357573a0068476ef4
Marco Meijer <marcomeijerm@gmail.com>
2023-12-01 11:53:24 +0100

    feat: copy for day2

---
+++ day2/src/bin/part1.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    println!("{output}");
+}
+
+fn part1(s: &str) -> String {
+    todo!();
+}
---
+++ day2/src/bin/part2.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let input = include_str!("./input2.txt");
+    let output = part2(input);
+    println!("{output}");
+}
+
+fn part2(s: &str) -> String {
+    todo!();
+}
